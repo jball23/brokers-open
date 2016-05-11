9cb21049a259b7ce751ea48a2570e51034bebc6e
Jordan Ballard <ballard.jordan@gmail.com>
2016-05-11 16:36:17 -0400

    added device geolocation so map centers on device location

--- brokers-open-angular/js/listings/controllers/index.controller.js
+++ brokers-open-angular/js/listings/controllers/index.controller.js
@@ -29,18 +29,48 @@
       })
     })
 
+
+    var initialLocation;
+    var siberia = new google.maps.LatLng(60, 105);
+    var newyork = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
+    var browserSupportFlag =  new Boolean();
     var map;
     this.initMap = function() {
 
         var mapOptions = {
           zoom: 8,
           center: {lat: 38.889931, lng: -77.009003}
-          // mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
+        // Try W3C Geolocation (Preferred)
+        if(navigator.geolocation) {
+          browserSupportFlag = true;
+          navigator.geolocation.getCurrentPosition(function(position) {
+            initialLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+            map.setCenter(initialLocation);
+          }, function() {
+            handleNoGeolocation(browserSupportFlag);
+          });
+        }
+        // Browser doesn't support Geolocation
+        else {
+          browserSupportFlag = false;
+          handleNoGeolocation(browserSupportFlag);
+        }
+
+        function handleNoGeolocation(errorFlag) {
+          if (errorFlag == true) {
+            alert("Geolocation service failed.");
+            initialLocation = newyork;
+          } else {
+            alert("Your browser doesn't support geolocation. We've placed you in Siberia.");
+            initialLocation = siberia;
+          }
+          map.setCenter(initialLocation);
+        }
       }
-  }
 
-
+    }
+    
 })();
