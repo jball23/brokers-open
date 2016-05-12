897919d31866ea5fbf92f344b4ce97a6bd9b3e96
Jordan Ballard <ballard.jordan@gmail.com>
2016-05-12 12:23:16 -0400

    removed underscores

--- brokers-open-angular/js/app.js
+++ brokers-open-angular/js/app.js
@@ -17,53 +17,53 @@
   $stateProvider
     .state("Index",{
       url: "/",
-      templateUrl:"js/templates/_index.html"
+      templateUrl:"js/templates/index.html"
     })
     .state("listingsIndex", {
     url: "/listings",
-    templateUrl: "js/listings/templates/_index.html",
+    templateUrl: "js/listings/templates/index.html",
     controller: "listingsIndexController",
     controllerAs: "listingsIndexVm"
     })
     .state("listingsNew",{
       url: "/listings/new",
-      templateUrl: "js/listings/templates/_new.html",
+      templateUrl: "js/listings/templates/new.html",
       controller: "listingsNewController",
       controllerAs:"listingsNewVm"
     })
     .state("listingsShow",{
       url: "/listings/:id",
-      templateUrl: "js/listings/templates/_show.html",
+      templateUrl: "js/listings/templates/show.html",
       controller: "listingsShowController",
       controllerAs: "listingsShowVm"
     })
     .state("listingsEdit",{
       url: "/listings/:id/edit",
-      templateUrl: "js/listings/templates/_edit.html",
+      templateUrl: "js/listings/templates/edit.html",
       controller: "listingsEditController",
       controllerAs: "listingsEditVm"
     })
     .state("usersIndex", {
       url: "/users",
-      templateUrl: "js/users/templates/_index.html",
+      templateUrl: "js/users/templates/index.html",
       controller: "usersIndexController",
       controllerAs: "usersIndexVm"
     })
     .state("usersNew",{
       url: "/users/new",
-      templateUrl: "js/users/templates/_new.html",
+      templateUrl: "js/users/templates/new.html",
       controller: "usersNewController",
       controllerAs: "usersNewVm"
     })
     .state("usersShow",{
       url: "/users/:id",
-      templateUrl: "js/users/templates/_show.html",
+      templateUrl: "js/users/templates/show.html",
       controller: "usersShowController",
       controllerAs: "usersShowVm"
     })
     .state("usersEdit",{
       url: "/users/:id/edit",
-      templateUrl: "js/users/templates/_edit.html",
+      templateUrl: "js/users/templates/edit.html",
       controller: "usersEditController",
       controllerAs:"usersEditVm"
     })
